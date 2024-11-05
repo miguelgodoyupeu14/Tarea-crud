f86db0dfbbd7cbd8e5323d7aa9b2d6b5742c08e6
miguelgodoyupeu14 <miguel.godoy@upeu.edu.pe>
2024-11-05 11:28:36 -0500

    v1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CarreraComponent } from './carrera/carrera.component';
+import { AlumnoComponent } from './alumno/alumno.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent,
+        title:'Home'
+    },
+    {
+        path:'carrera',
+        component: CarreraComponent,
+        title:'Soy carrera'
+    },
+    {
+        path:'alumno',
+        component:AlumnoComponent,
+        title:'Soy alumno'
+    },
+    {
+        path:'**',
+        redirectTo:'',
+        pathMatch:'full'
+    }
+];
